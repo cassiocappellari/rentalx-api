6c46d886b73283b5422d5ff48d5b47db7cc383b9
cassiocappellari <cassiocappellari@gmail.com>
2021-03-20 11:02:12 -0300

    feat: create server file with the first api route; config package.json with dev script

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,8 @@ import express from "express";
 
 const app = express();
 
-app.listen(3333);
+app.get("/", (req, res) => {
+  return res.json({ message: "Hello word!" });
+});
+
+app.listen(3333, () => console.log("server is running"));
