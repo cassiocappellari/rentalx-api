07a26e5b6777705f3f3beff6f578ecfc2de9a860
cassiocappellari <cassiocappellari@gmail.com>
2021-03-20 11:32:31 -0300

    feat: config vscode debugger at launch.json file

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,16 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   return res.json({ message: "Hello word!" });
+});
+
+app.post("/courses", (req, res) => {
+  const { name } = req.body;
+
+  return res.json({ name });
 });
 
 app.listen(3333, () => console.log("server is running"));
