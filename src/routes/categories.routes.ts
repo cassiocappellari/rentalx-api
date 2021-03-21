68a9f7165b0f53ca73abd047666aff95c81c84be
cassiocappellari <cassiocappellari@gmail.com>
2021-03-21 16:55:42 -0300

    feat: create categories routes file with create categories route

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+
+const categoriesRoutes = Router();
+
+const categories = [];
+
+categoriesRoutes.post("/categories", (req, res) => {
+  const { name, description } = req.body;
+
+  categories.push({
+    name,
+    description,
+  });
+
+  return res.status(201).send();
+});
+
+export { categoriesRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,10 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello word!" });
-});
-
-app.post("/courses", (req, res) => {
-  const { name } = req.body;
-
-  return res.json({ name });
-});
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log("server is running"));
