8f40820cddf16e04956f64882914253309f20a43
cassiocappellari <cassiocappellari@gmail.com>
2021-03-21 17:21:16 -0300

    feat: implement uuid lib at create categories route

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,20 @@
 import { Router } from "express";
+import { v4 as uuidV4 } from "uuid";
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post("/categories", (req, res) => {
+categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
 
-  categories.push({
+  const category = {
+    id: uuidV4(),
     name,
     description,
-  });
+  };
+
+  categories.push(category);
 
   return res.status(201).send();
 });
--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,6 @@ import { categoriesRoutes } from "./routes/categories.routes";
 const app = express();
 
 app.use(express.json());
-app.use(categoriesRoutes);
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => console.log("server is running"));
