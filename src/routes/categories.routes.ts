ef11000771ffbb78fc234f1c36f6b3a6dc001fc7
cassiocappellari <cassiocappellari@gmail.com>
2021-03-22 06:50:30 -0300

    feat: create Category model; implements Category class model at categories routes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,22 +1,25 @@
 import { Router } from "express";
-import { v4 as uuidV4 } from "uuid";
+
+import { Category } from "../model/Category";
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
 
-  const category = {
-    id: uuidV4(),
+  const category = new Category();
+
+  Object.assign(category, {
     name,
     description,
-  };
+    created_at: new Date(),
+  });
 
   categories.push(category);
 
-  return res.status(201).send();
+  return res.status(201).json({ category });
 });
 
 export { categoriesRoutes };
