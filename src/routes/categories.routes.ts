ff1c9c851b1e8d4aabb06c34b3e6843769b1de81
cassiocappellari <cassiocappellari@gmail.com>
2021-03-22 08:11:57 -0300

    feat: create and implement list categories route

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -16,4 +16,10 @@ categoriesRoutes.post("/", (req, res) => {
   return res.status(201).send();
 });
 
+categoriesRoutes.get("/", (req, res) => {
+  const categories = categoriesRepository.list();
+
+  return res.json(categories);
+});
+
 export { categoriesRoutes };
