e5ce04cf6148f244b83c73236b4e459512f5aabc
cassiocappellari <cassiocappellari@gmail.com>
2021-03-23 07:27:26 -0300

    feat: implement validation method in order to avoid duplicated category names

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,6 +8,12 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return res.status(400).json({ error: "category already exists" });
+  }
+
   categoriesRepository.create({
     name,
     description,
