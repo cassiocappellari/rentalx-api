653edad4cabec5f180113c9c9a6bf8109ce2894b
cassiocappellari <cassiocappellari@gmail.com>
2021-03-24 19:41:16 -0300

    refactor: send the responsability of create categories at post route to CreateCategoryService class

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -8,16 +9,8 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
-
-  if (categoryAlreadyExists) {
-    return res.status(400).json({ error: "category already exists" });
-  }
-
-  categoriesRepository.create({
-    name,
-    description,
-  });
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+  createCategoryService.execute({ name, description });
 
   return res.status(201).send();
 });
---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,25 @@
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateCategoryService {
+  constructor(private categoriesRepository: CategoriesRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+    if (categoryAlreadyExists) {
+      throw new Error("category already exists");
+    }
+
+    this.categoriesRepository.create({
+      name,
+      description,
+    });
+  }
+}
+
+export { CreateCategoryService };
