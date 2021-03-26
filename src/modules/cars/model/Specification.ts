fdc602a0d9ea6afb34da9839e4474067a4527c3e
cassiocappellari <cassiocappellari@gmail.com>
2021-03-26 19:33:29 -0300

    feat: create and implement car modules

---
+++ src/modules/cars/model/Specification.ts
@@ -0,0 +1,18 @@
+// arquivo referente a entidade/model de Specification
+
+import { v4 as uuidV4 } from "uuid";
+
+class Specification {
+  id?: string;
+  name: string;
+  description: string;
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { Specification };
---
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -0,0 +1,9 @@
+// arquivo referente ao serviço de criação de Specifications
+
+class CreateSpecificationService {
+  execute() {
+    console.log();
+  }
+}
+
+export { CreateSpecificationService };
