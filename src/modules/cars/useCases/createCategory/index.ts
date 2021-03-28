89daf9fecf349e0e6abdbf392a38c711fc16d90a
cassiocappellari <cassiocappellari@gmail.com>
2021-03-28 12:10:33 -0300

    refactor: refactor list categories route implementing controller and use case

--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -4,7 +4,6 @@ import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
 const categoriesRepository = new CategoriesRepository();
 const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
-
 const createCategoryController = new CreateCategoryController(
   createCategoryUseCase
 );
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,16 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req, res) => {
   return createCategoryController.handle(req, res);
 });
 
 categoriesRoutes.get("/", (req, res) => {
-  const categories = categoriesRepository.list();
-
-  return res.json(categories);
+  return listCategoriesController.handle(req, res);
 });
 
 export { categoriesRoutes };
