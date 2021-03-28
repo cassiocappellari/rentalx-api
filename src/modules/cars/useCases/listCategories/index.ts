9d492ee7265a19d53a2e66d1190c32cc1f06abb5
cassiocappellari <cassiocappellari@gmail.com>
2021-03-28 16:00:32 -0300

    feat: create a global class using singleton pattern at categories repository

--- src/modules/cars/useCases/listCategories/index.ts
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -2,10 +2,8 @@ import { CategoriesRepository } from "../../repositories/CategoriesRepository";
 import { ListCategoriesController } from "./ListCategoriesController";
 import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
-const categoriesRepository = new CategoriesRepository();
-
+const categoriesRepository = CategoriesRepository.getInstance();
 const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository);
-
 const listCategoriesController = new ListCategoriesController(
   listCategoriesUseCase
 );
