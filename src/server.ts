8d3fb62153cf41821756c265805481d7721b9340
cassiocappellari <cassiocappellari@gmail.com>
2021-04-02 17:12:39 -0300

    feat: install swagger tool; create swagger documentation file

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
 app.listen(3333, () => console.log("server is running"));
