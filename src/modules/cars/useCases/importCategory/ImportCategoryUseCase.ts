7fe52e2220ef15fc1c2c6f5c209c59c8b41e2ef5
cassiocappellari <cassiocappellari@gmail.com>
2021-04-02 18:19:32 -0300

    feat: update import category use case implementing a delete csv method after import data in the application

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -30,6 +30,7 @@ class ImportCategoryUseCase {
           });
         })
         .on("end", () => {
+          fs.promises.unlink(file.path);
           resolve(categories);
         })
         .on("error", (err) => {
