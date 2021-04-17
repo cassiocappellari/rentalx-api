d908632bc08c882ece67085dfbc363371b7db990
cassiocappellari <cassiocappellari@gmail.com>
2021-04-17 19:40:01 -0300

    feat: refactor create category use case

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -5,10 +5,10 @@ import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 class CreateCategoryController {
   constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
 
-  handle(req: Request, res: Response): Response {
+  async handle(req: Request, res: Response): Promise<Response> {
     const { name, description } = req.body;
 
-    this.createCategoryUseCase.execute({
+    await this.createCategoryUseCase.execute({
       name,
       description,
     });
--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -1,8 +1,8 @@
-import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
+// import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
 import { ImportCategoryController } from "./ImportCategoryController";
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
-const categoriesRepository = CategoriesRepository.getInstance();
+const categoriesRepository = null;
 const importCategoriesUseCase = new ImportCategoryUseCase(categoriesRepository);
 const importCategoryController = new ImportCategoryController(
   importCategoriesUseCase
