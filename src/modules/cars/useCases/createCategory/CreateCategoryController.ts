548d77d0ffdfec9ce2f1e1d841742e8fa6e7993f
cassiocappellari <cassiocappellari@gmail.com>
2021-04-21 18:46:08 -0300

    feat: install tsyringe lib and implement dependency injection at create categories use case

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -1,14 +1,15 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
 class CreateCategoryController {
-  constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
-
   async handle(req: Request, res: Response): Promise<Response> {
     const { name, description } = req.body;
 
-    await this.createCategoryUseCase.execute({
+    const createCategoryUseCase = container.resolve(CreateCategoryUseCase);
+
+    await createCategoryUseCase.execute({
       name,
       description,
     });
