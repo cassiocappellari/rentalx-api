498f2ebbea4e21ba832f7e5dcb22173ec31fa1b8
cassiocappellari <cassiocappellari@gmail.com>
2021-04-24 19:30:56 -0300

    feat: implement bcrypt hash method at user password; create find by email method at user repositories

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcrypt";
 import { inject, injectable } from "tsyringe";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
@@ -16,9 +17,17 @@ class CreateUserUseCase {
     email,
     drive_license,
   }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("user already exists");
+    }
+
+    const passwordHash = await hash(password, 8);
+
     await this.usersRepository.create({
       name,
-      password,
+      password: passwordHash,
       email,
       drive_license,
     });
