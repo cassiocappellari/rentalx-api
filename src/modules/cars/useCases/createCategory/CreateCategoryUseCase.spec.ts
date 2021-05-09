fcf2051e523706a915e1a634277185813b6ba5a0
cassiocappellari <cassiocappellari@gmail.com>
2021-05-09 19:39:52 -0300

    feat: install and configure Jest lib; create first test at create category use case

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,15 @@
+describe("Create category", () => {
+  it("2 + 2 = 4", () => {
+    const sum = 2 + 2;
+    const result = 4;
+
+    expect(sum).toBe(result);
+  });
+
+  it("2 + 2 != 5", () => {
+    const sum = 2 + 2;
+    const result = 5;
+
+    expect(sum).not.toBe(result);
+  });
+});
